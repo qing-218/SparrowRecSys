6631726ec7711c471d45f97de9f05b9e41e60bdc
qing-218 <bao.yiting@outlook.com>
2024-11-21 00:14:59 +0800

    Update DeepFM_v2.py

--- TFRecModel/src/com/sparrowrecsys/offline/tensorflow/DeepFM_v2.py
+++ TFRecModel/src/com/sparrowrecsys/offline/tensorflow/DeepFM_v2.py
@@ -85,6 +85,7 @@ item_genre_ind_col = tf.feature_column.indicator_column(item_genre_col)
 item_genre_emb_col = tf.feature_column.embedding_column(item_genre_col, 10)
 
 # fm first-order categorical items
+# FM模型中的一阶类别特征
 cat_columns = [movie_ind_col, user_ind_col, user_genre_ind_col, item_genre_ind_col]
 
 deep_columns = [tf.feature_column.numeric_column('releaseYear'),
@@ -94,7 +95,7 @@ deep_columns = [tf.feature_column.numeric_column('releaseYear'),
                 tf.feature_column.numeric_column('userRatingCount'),
                 tf.feature_column.numeric_column('userAvgRating'),
                 tf.feature_column.numeric_column('userRatingStddev')]
-
+# 一阶特征的类别特征部分和深度部分
 first_order_cat_feature = tf.keras.layers.DenseFeatures(cat_columns)(inputs)
 first_order_cat_feature = tf.keras.layers.Dense(1, activation=None)(first_order_cat_feature)
 first_order_deep_feature = tf.keras.layers.DenseFeatures(deep_columns)(inputs)
@@ -102,7 +103,7 @@ first_order_deep_feature = tf.keras.layers.Dense(1, activation=None)(first_order
 ## first order feature
 
 first_order_feature = tf.keras.layers.Add()([first_order_cat_feature, first_order_deep_feature])
-
+# 第二阶特征的类别嵌入部分
 second_order_cat_columns_emb = [tf.keras.layers.DenseFeatures([item_genre_emb_col])(inputs),
                                 tf.keras.layers.DenseFeatures([movie_emb_col])(inputs),
                                 tf.keras.layers.DenseFeatures([user_genre_emb_col])(inputs),
@@ -114,7 +115,8 @@ for feature_emb in second_order_cat_columns_emb:
     feature = tf.keras.layers.Dense(64, activation=None)(feature_emb)
     feature = tf.keras.layers.Reshape((-1, 64))(feature)
     second_order_cat_columns.append(feature)
-
+    
+# 第二阶特征的深度部分
 second_order_deep_columns = tf.keras.layers.DenseFeatures(deep_columns)(inputs)
 second_order_deep_columns = tf.keras.layers.Dense(64, activation=None)(second_order_deep_columns)
 second_order_deep_columns = tf.keras.layers.Reshape((-1, 64))(second_order_deep_columns)
@@ -125,23 +127,22 @@ deep_feature = tf.keras.layers.Flatten()(second_order_fm_feature)
 deep_feature = tf.keras.layers.Dense(32, activation='relu')(deep_feature)
 deep_feature = tf.keras.layers.Dense(16, activation='relu')(deep_feature)
 
-
+# 自定义ReduceLayer层，用于对输入进行聚合操作（可以选择sum或mean）
 class ReduceLayer(tf.keras.layers.Layer):
     def __init__(self, axis, op='sum', **kwargs):
         super().__init__()
         self.axis = axis
         self.op = op
-        assert self.op in ['sum', 'mean']
-
+        assert self.op in ['sum', 'mean'] # 限制操作类型只能是 'sum' 或 'mean'
     def build(self, input_shape):
         pass
 
     def call(self, input, **kwargs):
         if self.op == 'sum':
-            return tf.reduce_sum(input, axis=self.axis)
+            return tf.reduce_sum(input, axis=self.axis)  # 沿指定轴求和
         elif self.op == 'mean':
-            return tf.reduce_mean(input, axis=self.axis)
-        return tf.reduce_sum(input, axis=self.axis)
+            return tf.reduce_mean(input, axis=self.axis)# 沿指定轴求均值
+        return tf.reduce_sum(input, axis=self.axis)# 默认为求和操作
 
 
 second_order_sum_feature = ReduceLayer(1)(second_order_fm_feature)
@@ -149,9 +150,12 @@ second_order_sum_square_feature = tf.keras.layers.multiply([second_order_sum_fea
 second_order_square_feature = tf.keras.layers.multiply([second_order_fm_feature, second_order_fm_feature])
 second_order_square_sum_feature = ReduceLayer(1)(second_order_square_feature)
 ## second_order_fm_feature
+# 计算二阶FM特征
 second_order_fm_feature = tf.keras.layers.subtract([second_order_sum_square_feature, second_order_square_sum_feature])
-
+# 合并一阶特征、二阶FM特征和深度特征
 concatenated_outputs = tf.keras.layers.Concatenate(axis=1)([first_order_feature, second_order_fm_feature, deep_feature])
+
+# 输出层，使用Sigmoid激活函数进行二分类
 output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(concatenated_outputs)
 
 model = tf.keras.Model(inputs, output_layer)
