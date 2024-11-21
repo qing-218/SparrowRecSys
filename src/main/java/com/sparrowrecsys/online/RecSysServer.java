be450ce52e601c7ab3eaedd8759735bf38e6511e
Easha-W <2556296549@qq.com>
2024-11-21 10:53:41 +0800

    Update RecSysServer.java

--- src/main/java/com/sparrowrecsys/online/RecSysServer.java
+++ src/main/java/com/sparrowrecsys/online/RecSysServer.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.net.URL;
 
 /***
- * Recsys Server, end point of online recommendation service
+ * 推荐系统服务器，在线推荐服务的终端
  */
 
 public class RecSysServer {
@@ -21,7 +21,7 @@ public class RecSysServer {
         new RecSysServer().run();
     }
 
-    //recsys server port number
+    // 推荐系统服务器端口号
     private static final int DEFAULT_PORT = 6010;
 
     public void run() throws Exception{
@@ -31,36 +31,36 @@ public class RecSysServer {
             port = Integer.parseInt(System.getenv("PORT"));
         } catch (NumberFormatException ignored) {}
 
-        //set ip and port number
+        // 设置 IP 和端口号
         InetSocketAddress inetAddress = new InetSocketAddress("0.0.0.0", port);
         Server server = new Server(inetAddress);
 
-        //get index.html path
+        // 获取 index.html 的路径
         URL webRootLocation = this.getClass().getResource("/webroot/index.html");
         if (webRootLocation == null)
         {
             throw new IllegalStateException("Unable to determine webroot URL location");
         }
 
-        //set index.html as the root page
+        // 将 index.html 设置为根页面
         URI webRootUri = URI.create(webRootLocation.toURI().toASCIIString().replaceFirst("/index.html$","/"));
         System.out.printf("Web Root URI: %s%n", webRootUri.getPath());
 
-        //load all the data to DataManager
+       // 加载所有数据到 DataManager 中
         DataManager.getInstance().loadData(webRootUri.getPath() + "sampledata/movies.csv",
                 webRootUri.getPath() + "sampledata/links.csv",webRootUri.getPath() + "sampledata/ratings.csv",
                 webRootUri.getPath() + "modeldata/item2vecEmb.csv",
                 webRootUri.getPath() + "modeldata/userEmb.csv",
                 "i2vEmb", "uEmb");
 
-        //create server context
+       // 创建服务器上下文
         ServletContextHandler context = new ServletContextHandler();
-        context.setContextPath("/");
-        context.setBaseResource(Resource.newResource(webRootUri));
-        context.setWelcomeFiles(new String[] { "index.html" });
-        context.getMimeTypes().addMimeMapping("txt","text/plain;charset=utf-8");
+        context.setContextPath("/");// 设置上下文路径为 "/"，即根路径
+        context.setBaseResource(Resource.newResource(webRootUri));// 将资源的根目录设置为 webRootUri
+        context.setWelcomeFiles(new String[] { "index.html" });// 配置默认欢迎文件为 "index.html"
+        context.getMimeTypes().addMimeMapping("txt","text/plain;charset=utf-8");// 添加文件的 MIME 类型映射
 
-        //bind services with different servlets
+        // 绑定不同服务到相应的 Servlet
         context.addServlet(DefaultServlet.class,"/");
         context.addServlet(new ServletHolder(new MovieService()), "/getmovie");
         context.addServlet(new ServletHolder(new UserService()), "/getuser");
@@ -68,12 +68,13 @@ public class RecSysServer {
         context.addServlet(new ServletHolder(new RecommendationService()), "/getrecommendation");
         context.addServlet(new ServletHolder(new RecForYouService()), "/getrecforyou");
 
-        //set url handler
+        // 设置 URL 处理器
         server.setHandler(context);
         System.out.println("RecSys Server has started.");
 
-        //start Server
+         // 启动服务器
         server.start();
+        // 保持服务器运行
         server.join();
     }
 }
