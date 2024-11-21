b3cdd3e9e971eca46a87de821be856d6096804ea
toutrethm <142793371+toutrethm@users.noreply.github.com>
2024-11-21 10:31:15 +0800

    Update MovieService.java

--- src/main/java/com/sparrowrecsys/online/service/MovieService.java
+++ src/main/java/com/sparrowrecsys/online/service/MovieService.java
@@ -10,30 +10,46 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * MovieService, return information of a specific movie
+ * MovieService，返回特定电影的信息
  */
-
 public class MovieService extends HttpServlet {
+    /**
+     * 处理GET请求
+     * <p>
+     * 这个方法处理HTTP GET请求，用于返回特定电影的详细信息。
+     * 它首先设置响应的内容类型为JSON，然后从请求的URL参数中获取电影ID。
+     * 使用DataManager获取电影对象，并将该对象转换为JSON格式返回给客户端。
+     * 如果电影对象不存在，则返回一个空字符串。
+     * </p>
+     *
+     * @param request  HTTP请求对象
+     * @param response HTTP响应对象
+     * @throws IOException 如果发生I/O错误
+     */
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws IOException {
         try {
+            // 设置响应的内容类型为JSON
             response.setContentType("application/json");
+            // 设置HTTP响应状态码为200（OK）
             response.setStatus(HttpServletResponse.SC_OK);
+            // 设置响应的字符编码为UTF-8
             response.setCharacterEncoding("UTF-8");
+            // 设置响应头允许跨域请求
             response.setHeader("Access-Control-Allow-Origin", "*");
 
-            //get movie id via url parameter
+            // 通过URL参数获取电影ID
             String movieId = request.getParameter("id");
 
-            //get movie object from DataManager
+            // 从DataManager获取电影对象
             Movie movie = DataManager.getInstance().getMovieById(Integer.parseInt(movieId));
 
-            //convert movie object to json format and return
+            // 将电影对象转换为JSON格式并返回
             if (null != movie) {
                 ObjectMapper mapper = new ObjectMapper();
                 String jsonMovie = mapper.writeValueAsString(movie);
                 response.getWriter().println(jsonMovie);
-            }else {
+            } else {
                 response.getWriter().println("");
             }
 
