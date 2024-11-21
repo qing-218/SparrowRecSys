d89e37f003d6cd08c19b4a5f5910ac09b57242a9
Easha-W <2556296549@qq.com>
2024-11-21 11:21:40 +0800

    Update UserService.java

--- src/main/java/com/sparrowrecsys/online/service/UserService.java
+++ src/main/java/com/sparrowrecsys/online/service/UserService.java
@@ -9,35 +9,48 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * UserService, return information of a specific user
+ * UserService 类，提供用户相关的服务，返回特定用户的信息
  */
 
 public class UserService extends HttpServlet {
+     /**
+     * 处理 GET 请求，返回指定用户的信息
+     * @param request HTTP 请求对象，包含用户的请求信息
+     * @param response HTTP 响应对象，用于返回用户信息
+     * @throws IOException 当输入或输出操作发生异常时抛出
+     */
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws IOException {
         try {
+            // 设置响应内容类型为 JSON 格式
             response.setContentType("application/json");
+            // 设置 HTTP 响应状态为 200 (OK)
             response.setStatus(HttpServletResponse.SC_OK);
+            // 设置响应的字符编码为 UTF-8
             response.setCharacterEncoding("UTF-8");
+            // 设置跨域访问头部，允许任意来源访问
             response.setHeader("Access-Control-Allow-Origin", "*");
 
-            //get user id via url parameter
+             // 从 URL 参数中获取用户 ID
             String userId = request.getParameter("id");
 
-            //get user object from DataManager
+            // 从 DataManager 获取对应的用户对象
             User user = DataManager.getInstance().getUserById(Integer.parseInt(userId));
 
-            //convert movie object to json format and return
+            // 将用户对象转换为 JSON 格式并返回
             if (null != user) {
-                ObjectMapper mapper = new ObjectMapper();
-                String jsonUser = mapper.writeValueAsString(user);
-                response.getWriter().println(jsonUser);
+                ObjectMapper mapper = new ObjectMapper();// JSON 工具类
+                String jsonUser = mapper.writeValueAsString(user);// 将用户对象转换为 JSON 字符串
+                response.getWriter().println(jsonUser); // 将 JSON 数据写入响应
             }else{
+                // 如果用户对象为 null，返回空字符串
                 response.getWriter().println("");
             }
 
         } catch (Exception e) {
+            // 捕获异常并打印堆栈信息
             e.printStackTrace();
+            // 返回空字符串作为响应
             response.getWriter().println("");
         }
     }
